81b321e8077b2e26ba2b714db29bcb570c1a00a0
Jun Song Park <jp6ax@virginia.edu>
2020-10-08 21:59:21 -0400

    google login

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="polls/index.html")),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
