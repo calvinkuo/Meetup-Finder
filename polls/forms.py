dd0957023fb326db8f0b86063bc0d7c9db195abe
Xinyuan Zhu <xz4x@virginia.edu>
2020-10-22 22:55:33 -0400

    Added location to form

--- polls/forms.py
+++ polls/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-
+from address.forms import AddressField
 
 class CommentForm(forms.Form):
     name_text = forms.CharField(label='Title', max_length=200)
     comment_text = forms.CharField(label='Comment', max_length=1000)
+    address = AddressField()
