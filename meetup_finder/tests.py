97abdd26c02aa22eba14a097c25e2a364b1a355a
calvinkuo <14984741+calvinkuo@users.noreply.github.com>
2020-10-25 23:10:20 -0400

    Updating tests after rename

--- meetup_finder/tests.py
+++ meetup_finder/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
-from .models import Comments
+from .models import Events
 
 
 def create_event(organizer="", name="", comment="", address="", days=0):
@@ -13,12 +13,12 @@ def create_event(organizer="", name="", comment="", address="", days=0):
     takes place `days` from now (negative for past event).
     """
     time = timezone.now() + datetime.timedelta(days=days)
-    return Comments.objects.create(organizer=organizer,
-                                   name=name,        # event name
-                                   comment=comment,  # event details
-                                   address=address,
-                                   event_date=time.date().isoformat(),
-                                   event_time=time.time().isoformat())
+    return Events.objects.create(organizer=organizer,
+                                 name=name,
+                                 comment=comment,  # event details
+                                 address=address,
+                                 event_date=time.date().isoformat(),
+                                 event_time=time.time().isoformat())
 
 
 class EventsIndexViewTests(TestCase):
@@ -26,7 +26,7 @@ class EventsIndexViewTests(TestCase):
         """
         If no events exist, an appropriate message is displayed.
         """
-        response = self.client.get(reverse('polls:index'))  # the list of events
+        response = self.client.get(reverse('meetup_finder:index'))  # the list of events
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No events are available.")
         self.assertQuerysetEqual(response.context['comment_list'], [])  # the list of events
@@ -36,10 +36,10 @@ class EventsIndexViewTests(TestCase):
         An added event is displayed on the events page.
         """
         create_event(organizer="Test Organizer",
-                     name="Test Event Name",  # event name
+                     name="Test Event Name",
                      comment="Test Event Details",  # event details
                      address="Test Address")
-        response = self.client.get(reverse('polls:index'))
+        response = self.client.get(reverse('meetup_finder:index'))
         self.assertEqual(response.status_code, 200)
         # self.assertContains(response, "Test Organizer")
         self.assertContains(response, "Test Event Name")
@@ -51,14 +51,14 @@ class EventsIndexViewTests(TestCase):
         A second added event is displayed on the events page.
         """
         create_event(organizer="Test Organizer",
-                     name="Test Event Name",  # event name
+                     name="Test Event Name",
                      comment="Test Event Details",  # event details
                      address="Test Address")
         create_event(organizer="Test Organizer 2",
-                     name="Test Event Name 2",  # event name
+                     name="Test Event Name 2",
                      comment="Test Event Details 2",  # event details
                      address="Test Address 2")
-        response = self.client.get(reverse('polls:index'))
+        response = self.client.get(reverse('meetup_finder:index'))
         # self.assertContains(response, "Test Organizer 2")
         self.assertContains(response, "Test Event Name 2")
         self.assertContains(response, "Test Event Details 2")
@@ -70,7 +70,7 @@ class EventsCreateViewTests(TestCase):
         """
         Check that create event form works.
         """
-        response = self.client.get(reverse('polls:comments'))  # the list of events
+        response = self.client.get(reverse('meetup_finder:events'))  # the list of events
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Organizer")
         self.assertContains(response, "Event Name")
